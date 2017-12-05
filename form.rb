1a7cc4aac79c3c85b6ba272daa6a8007e211f657
piotrjm <piotrjm@gmail.com>
2017-12-05 23:09:38 +0100

    "Borders and merging cells loops"

--- form.rb
+++ form.rb
@@ -72,13 +72,11 @@ column_width = [
 
 #column width setter - values taken from array column_width
 xx = 0
-xy = 0
 yy = 0
-yz = 1
 
 while xx < column_width.length
-	a_cw = column_width[xx][xy];
-	b_cw = column_width[yy][yz];
+	a_cw = column_width[xx][0];
+	b_cw = column_width[yy][1];
 	sheet.change_column_width(a_cw, b_cw)
 	xx += 1
 	yy += 1
@@ -86,116 +84,84 @@ end
 
 workbook.write("./form.xlsx")
 
+num_a = 1
+num_b = 6
 
+num_x = 0
+num_y = 93
 
-#merge columns - head rows color 'D9D9D9'
-sheet.merge_cells(0, 1, 0, 6)  # Merges B1:G1
-sheet.merge_cells(1, 1, 1, 6)  # Merges B2:G2
-sheet.merge_cells(3, 1, 3, 6)  # Merges B4:G4
-sheet.merge_cells(4, 1, 4, 2)  # Merges B5:C5
-sheet.merge_cells(4, 3, 4, 6)  # Merges D5:G5
-sheet.merge_cells(5, 1, 5, 2)  
-sheet.merge_cells(5, 3, 5, 6)  
-sheet.merge_cells(6, 1, 6, 2)  
-sheet.merge_cells(6, 3, 6, 6)  
-sheet.merge_cells(7, 1, 7, 2)  
-sheet.merge_cells(7, 3, 7, 6)  
-sheet.merge_cells(8, 1, 8, 2)  
-sheet.merge_cells(8, 3, 8, 5) 
-sheet.merge_cells(9, 1, 9, 2)  
-sheet.merge_cells(9, 3, 9, 5) 
-# sheet.merge_cells(10, 1, 10, 2)  
-# sheet.merge_cells(10, 3, 10, 5) 
-# sheet.merge_cells(11, 1, 11, 2)  
-# sheet.merge_cells(11, 3, 11, 5) 
-sheet.merge_cells(12, 1, 12, 2)  
-sheet.merge_cells(12, 3, 12, 6) 
-sheet.merge_cells(14, 1, 14, 6)
-sheet.merge_cells(16, 2, 16, 5)
-sheet.merge_cells(17, 2, 17, 5)
-sheet.merge_cells(18, 1, 18, 6)
-#sheet.merge_cells(19, 1, 19, 6)
-#sheet.merge_cells(20, 1, 20, 6)
-#sheet.merge_cells(21, 1, 21, 6)
-#sheet.merge_cells(22, 1, 22, 6)
-sheet.merge_cells(23, 2, 23, 5)
-sheet.merge_cells(24, 1, 24, 6)
-# sheet.merge_cells(25, 1, 25, 6)
-# sheet.merge_cells(26, 1, 26, 6)
-# sheet.merge_cells(27, 1, 27, 6)
-# sheet.merge_cells(28, 1, 28, 6)
-sheet.merge_cells(29, 2, 29, 5)
-sheet.merge_cells(30, 1, 30, 6)
-# sheet.merge_cells(31, 1, 31, 6)
-# sheet.merge_cells(32, 1, 32, 6)
-# sheet.merge_cells(33, 1, 33, 6)
-# sheet.merge_cells(34, 1, 34, 6)
-sheet.merge_cells(35, 2, 35, 5)
-sheet.merge_cells(36, 1, 36, 6)
-# sheet.merge_cells(37, 1, 37, 6)
-# sheet.merge_cells(38, 1, 38, 6)
-# sheet.merge_cells(39, 1, 39, 6)
-# sheet.merge_cells(40, 1, 40, 6)
-sheet.merge_cells(41, 2, 41, 5)
-sheet.merge_cells(42, 1, 42, 6)
-# sheet.merge_cells(43, 1, 43, 6)
-# sheet.merge_cells(44, 1, 44, 6)
-# sheet.merge_cells(45, 1, 45, 6)
-# sheet.merge_cells(46, 1, 46, 6)
-
-sheet.merge_cells(48, 2, 48, 5)
-sheet.merge_cells(49, 2, 49, 5)
-
-sheet.merge_cells(51, 2, 51, 6)
-sheet.merge_cells(52, 2, 52, 6)
-sheet.merge_cells(53, 2, 53, 6)
-sheet.merge_cells(54, 2, 54, 6)
-
-sheet.merge_cells(56, 1, 56, 6)
-sheet.merge_cells(57, 2, 57, 5)
-sheet.merge_cells(58, 2, 58, 5)
-sheet.merge_cells(59, 1, 59, 6)
-sheet.merge_cells(60, 1, 60, 6)
-sheet.merge_cells(61, 1, 61, 6)
-sheet.merge_cells(62, 1, 62, 6)
-sheet.merge_cells(63, 1, 63, 6)
-
-#final decision
-sheet.merge_cells(65, 2, 65, 6)
-sheet.merge_cells(66, 2, 66, 4)
-sheet.merge_cells(66, 5, 66, 6)
-sheet.merge_cells(67, 1, 67, 6)
-sheet.merge_cells(68, 1, 68, 6)
-sheet.merge_cells(69, 1, 69, 6)
-sheet.merge_cells(70, 1, 70, 6)
-sheet.merge_cells(71, 1, 71, 6)
-
-#first evaluator
-sheet.merge_cells(73, 1, 73, 2)
-sheet.merge_cells(73, 3, 73, 6)
-sheet.merge_cells(74, 1, 74, 2)
-sheet.merge_cells(74, 3, 74, 6)
-sheet.merge_cells(75, 1, 75, 2)
-sheet.merge_cells(75, 3, 75, 6)
-
-#second evaluator
-sheet.merge_cells(77, 1, 77, 2)
-sheet.merge_cells(77, 3, 77, 6)
-sheet.merge_cells(78, 1, 78, 2)
-sheet.merge_cells(78, 3, 78, 6)
-sheet.merge_cells(79, 1, 79, 2)
-sheet.merge_cells(79, 3, 79, 6)
-
-#third evaluator
-sheet.merge_cells(80, 1, 80, 2)
-sheet.merge_cells(80, 3, 80, 6)
-sheet.merge_cells(81, 1, 81, 2)
-sheet.merge_cells(81, 3, 81, 6)
-sheet.merge_cells(82, 1, 82, 2)
-sheet.merge_cells(82, 3, 82, 6)
-
+for y_y in num_a..num_b
+  for x_x in num_x..num_y
+	  c = workbook[0].add_cell(x_x, y_y)
+	  c.change_border(:bottom, 'thin')
+		c.change_border(:top, 'thin')
+		c.change_border(:right, 'thin')
+		c.change_border(:left, 'thin')
+	end
+end
 
 workbook.write("./form.xlsx")
+
+no_border_rows = [2, 13, 14, 15, 47, 56, 57, 65, 74, 82, 86, 90]
+
+num_a = 1
+num_b = 6
+
+for y_y in num_a..num_b
+	no_border_rows.each do |row|
+		c = workbook[0].add_cell(row, y_y)
+	  c.change_border(:bottom, 'hairline')
+		c.change_border(:top, 'hairline')
+		c.change_border(:right, 'hairline')
+		c.change_border(:left, 'hairline')
+	end
+end
+
+workbook.write("./form.xlsx")
+
+#merge columns - head rows color 'D9D9D9'
+cells_to_merge = [[0, 1, 0, 6], [1, 1, 1, 6], [3, 1, 3, 6], [4, 1, 4, 2], [4, 3, 4, 6], [5, 1, 5, 2], [5, 3, 5, 6],
+							[6, 1, 6, 2], [6, 3, 6, 6], [7, 1, 7, 2], [7, 3, 7, 6], [8, 1, 8, 2], [8, 3, 8, 5], [9, 1, 9, 2],
+							[9, 3, 9, 5], [12, 1, 12, 2], [12, 3, 12, 6], [14, 1, 14, 6], [16, 2, 16, 5], [17, 2, 17, 5],
+							[18, 1, 18, 6], [23, 2, 23, 5], [24, 1, 24, 6], [29, 2, 29, 5], [30, 1, 30, 6], [35, 2, 35, 5],
+							[36, 1, 36, 6], [41, 2, 41, 5], [42, 1, 42, 6], [48, 2, 48, 5], [49, 2, 49, 5], [51, 2, 51, 6],
+							[52, 2, 52, 6], [53, 2, 53, 6], [54, 2, 54, 6], [56, 1, 56, 6], [57, 2, 57, 5], [58, 2, 58, 5],
+							[59, 1, 59, 6], [60, 1, 60, 6], [61, 1, 61, 6], [62, 1, 62, 6], [63, 1, 63, 6], [65, 2, 65, 6],
+							[66, 2, 66, 4], [66, 5, 66, 6], [67, 1, 67, 6], [68, 1, 68, 6], [69, 1, 69, 6], [70, 1, 70, 6],
+							[71, 1, 71, 6], [73, 1, 73, 2], [73, 3, 73, 6], [74, 1, 74, 2], [74, 3, 74, 6], [75, 1, 75, 2],
+							[75, 3, 75, 6], [77, 1, 77, 2], [77, 3, 77, 6], [78, 1, 78, 2], [78, 3, 78, 6], [79, 1, 79, 2],
+							[79, 3, 79, 6], [80, 1, 80, 2], [80, 3, 80, 6], [81, 1, 81, 2], [81, 3, 81, 6], [82, 1, 82, 2],
+							[82, 3, 82, 6]]
+
+	  
+ctm1 = 0
+
+while ctm1 < cells_to_merge.length
+	a_cw = cells_to_merge[ctm1][0];
+	b_cw = cells_to_merge[ctm1][1];
+	c_cw = cells_to_merge[ctm1][2];
+	d_cw = cells_to_merge[ctm1][3];
+	sheet.merge_cells(a_cw, b_cw, c_cw, d_cw)
+	ctm1 += 1
+end
+
+workbook.write("./form.xlsx")
+
+
+rows_height = [
+		[0, 27.75], [1, 129], [2, 18], [3, 18], [4, 123.5], [5, 57], [6, 57], [7, 18],
+		[8, 18],[9, 18], [10, 24], [11, 24], [12, 18], [13, 18], [14, 36], [15, 18],
+		[16, 18], [17, 18], [18, 18], [19, 18], [20, 18], [21, 18], [22, 18], [23, 72],
+		[24, 18], [25, 18], [26, 18], [27, 18], [28, 18], [29, 36], [30, 18], [31, 18],
+		[32, 18], [33, 18], [34, 18], [35, 72], [36, 18], [37, 18], [38, 18], [39, 18],
+		[40, 18], [41, 18], [42, 18], [43, 18], [44, 18], [45, 18], [46, 0], [47, 0],
+		[48, 0], [49, 0], [50, 0], [51, 0], [52, 18], [53, 0], [54, 18], [55, 57], [56, 18],
+		[57, 18], [58, 18], [59, 18], [60, 18], [61, 18], [62, 18], [63, 18], [64, 18], 
+		[65, 18], [66, 18], [67, 36], [68, 36], [69, 18], [70, 18], [71, 18], [72, 18], 
+		[73, 18], [74, 18], [75, 18], [76, 36], [77, 18], [78, 18], [79, 18], [80, 18], 
+		[81, 18], [82, 18], [83, 36], [84, 18], [85, 36], [86, 18], [87, 36], [88, 18], 
+		[89, 36], [90, 18], [91, 36], [92, 18], [93, 36]
+	]
 
 #set rows height
 sheet.change_row_height(0, 27.75)  # Sets first row height to 27.75
